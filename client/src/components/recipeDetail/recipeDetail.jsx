630ade90593bc2ad3671cdd5797be9c0200d8e7b
Paul C <pecardozo17@gmail.com>
2022-09-15 17:39:20 -0300

    Added some css for the recipe Detail

--- client/src/components/recipeDetail/recipeDetail.jsx
+++ client/src/components/recipeDetail/recipeDetail.jsx
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import { useDispatch,useSelector } from "react-redux";
 import { getOneRecipe } from "../../actions";
 import {useParams} from 'react-router-dom';
+import styles from './recipeDetail.module.css';
 
 export default function RecipeDetail(){
     const detail = useSelector(state=>state.detail);
@@ -13,27 +14,42 @@ export default function RecipeDetail(){
     },[])
 
     return(
-        <div>
+        <div >
             {Object.keys(detail).length>0 && 
-            <div>
-                <h3>{detail.name}</h3>
-                <img src={detail.picture} alt='Food'/>
-                <h3>Healthyness: {detail.healthScore}</h3>
-                {detail.dietTypes.map((element, index)=>{
-                    return<p
-                    id='dietType'
-                    key={index}>{element}</p>
-                })}
-                {detail.dishTypes.map((element, index)=>{
-                    return<p
-                    id='dishType'
-                    key={index}>{element}</p>
-                })}
-                <div>
+            <div className={styles.container}>
+                <h3 className={styles.title} >{detail.name}</h3>
+                <img className={styles.img} src={detail.picture} alt='Food'/>
+                <h3 className={styles.hScore}>Healthyness Score: {detail.healthScore}</h3>
+                <div className={styles.dietSctn}>
+                    <h3 className={styles.dietTypeTitle}>Diet Types</h3>
+                    <div className={styles.dietsContainer}>
+                        {detail.dietTypes.map((element, index)=>{
+                            return<p
+                            className={styles.dietTag}
+                            id='dietType'
+                            key={index}>{element}</p>
+                        })}
+                    </div>
+                </div>
+                <br></br>
+                <div className={styles.dietSctn}>
+                    <h3 className={styles.dietTypeTitle}>Dish Types</h3>
+                    <div className={styles.dietsContainer}>
+                        {detail.dishTypes.map((element, index)=>{
+                            return<p
+                            className={styles.dietTag}
+                            id='dishType'
+                            key={index}>{element}</p>
+                        })}
+                    </div>
+                </div>
+                <br></br>
+                <div className={styles.dietSctn}>
                     <h3>Summary</h3>
                     <p>{detail.summary}</p>
                 </div>
-                <div>
+                <br></br>
+                <div className={styles.dietSctn}>
                     <h3>Step by step</h3>
                     <p>{detail.steps}</p>
                 </div>
